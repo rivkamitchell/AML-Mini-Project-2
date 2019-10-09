f613d4226e23a545ac48b9486ed32312d1a20600
yayabambam <39013122+yayabambam@users.noreply.github.com>
2019-10-09 14:55:26 -0400

    Update NAIVE.py

--- NAIVE.py
+++ NAIVE.py
@@ -1,22 +1,27 @@
 import numpy as np
-#no tested 
 
-class NAIVE:
 
-    def __init__(self, X, x_range, y_range): #x_range should be a discrete set(binary for now) as well as y_range
+
+
+class Naive:
+
+    def __init__(self, X, y_range): #x_range should be a discrete set(binary for now) as well as y_range
         X=np.array(X)
         self.data = X
         self.validation=X
         (self.num_exp, self.num_features)=X.shape
+        self.num_features-=1 #the result was counted in
+
         self.features = X[:,:len(X[0])-1]
         self.results=X[:, -1]
         self.x_range=2
         self.y_range=y_range
+        print("yaaaaaa")
         (self.theta_vect, self.theta_mat)=self.fit()
 
     def fit(self):
         #calculates the theta matrix 
-        theta=np.zeros(self.num_features, self.y_range)
+        theta=np.zeros((self.num_features, self.y_range))
         vector=np.zeros(self.y_range) #prob of y=p
 
         y_list=[]
@@ -28,11 +33,11 @@ class NAIVE:
         for row in self.data:
            y_list[row[-1]]+=[row]
 
-        for p in range(y_range):
+        for p in range(self.y_range):
             tot=len(y_list[p])
             vector[p]=tot/self.num_exp #prob of y=p
             if tot!=0:
-                for i in range(slef.num_features):
+                for i in range(self.num_features):
                     num_of_i=0
                     for row in y_list[p]:
                         if row[i]==1:
@@ -49,6 +54,8 @@ class NAIVE:
         max_class=0
         for p in range(self.y_range):
             prob=self.theta_vect[p]
+            if prob==0:
+                print("kawabnunga")
             for i in range(self.num_features):
                 if vect[i]==1:
                     prob*=self.theta_mat[i, p]
@@ -82,6 +89,6 @@ class NAIVE:
             temp2=(i+1)*size
             train=np.vstack((self.data[:temp1], self.data[temp2:]))
             vali=self.data[temp1:temp2]
-            model=NAIVE(train, self.x_range, self.y_range)
+            model=Naive(train, self.y_range)
             acc+=model.accuracy(vali)
         return (acc/k)
