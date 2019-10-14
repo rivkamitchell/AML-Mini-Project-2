8a076ed795cfdc44c5130c89953f98cbf90c1516
Rivka Mitchell <rivkamitchell@mail.mcgill.ca>
2019-10-13 20:04:42 -0400

    #

--- SCIKIT.py
+++ SCIKIT.py
@@ -34,35 +34,35 @@ def majority_vote(train_data, test_data):
     tfidf_transformer = TfidfTransformer()
     X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 
-    Multinomial_Classification = MultinomialNB().fit(X_train_tfidf, y_train)
+    Multinomial_Classification = MultinomialNB(alpha = 0.05).fit(X_train_tfidf, y_train)
     Bernoulli_Classification = BernoulliNB().fit(X_train_tfidf, y_train)
-    Decision_Trees_Classification = DecisionTreeClassifier(random_state = 0).fit(X_train_tfidf, y_train)
-    Logistic_Regression_Classification = LogisticRegression(random_state = 0, solver = 'lbfgs', multi_class = 'multinomial').fit(X_train_tfidf, y_train)
+    #Decision_Trees_Classification = DecisionTreeClassifier(random_state = 0).fit(X_train_tfidf, y_train)
+    Logistic_Regression_Classification = LogisticRegression(alpha = 0.25, random_state = 0, solver = 'lbfgs', multi_class = 'multinomial').fit(X_train_tfidf, y_train)
     
     test = Preprocessing.prune(test_data)
 
     mc = Multinomial_Classification.predict(count_vect.transform(test))
     bc = Bernoulli_Classification.predict(count_vect.transform(test))
-    dt = Decision_Trees_Classification.predict(count_vect.transform(test))
+    #dt = Decision_Trees_Classification.predict(count_vect.transform(test))
     lr = Logistic_Regression_Classification.predict(count_vect.transform(test))
     
-    votes = np.array([mc, bc, dt, lr]).T
+    votes = np.array([mc, bc, lr]).T
 
     predictions = []
     for i in range(0, len(test)):
         vote_0 = votes[i][0]
         vote_1 = votes[i][1]
         vote_2 = votes[i][2]
-        vote_3 = votes[i][3]
+        #vote_3 = votes[i][3]
     
-        col_votes = [vote_0, vote_1, vote_2, vote_3]
+        col_votes = [vote_0, vote_1, vote_2]
 
         count_0 = np.count_nonzero(votes[i] == vote_0)
         count_1 = np.count_nonzero(votes[i] == vote_1)
         count_2 = np.count_nonzero(votes[i] == vote_2)
-        count_3 = np.count_nonzero(votes[i] == vote_3)
+        #count_3 = np.count_nonzero(votes[i] == vote_3)
 
-        counts = [count_0, count_1, count_2, count_3]
+        counts = [count_0, count_1, count_2]
         
         maj_vote = col_votes[0]
         maj_count = counts[0]
@@ -91,7 +91,7 @@ def predict(train_data, test_data, model):
         return Bernoulli_Classification.predict(count_vect.transform(test))
         
     if model == 'Multinomial':
-        Multinomial_Classification = MultinomialNB().fit(X_train_tfidf, y_train)
+        Multinomial_Classification = MultinomialNB(alpha = 0.05).fit(X_train_tfidf, y_train)
         return Multinomial_Classification.predict(count_vect.transform(test))
     
     if model == 'DecisionTree':
@@ -107,16 +107,16 @@ def predict(train_data, test_data, model):
 
 
 multinomial_predictions = predict(train_data, test_data, 'Multinomial')
-bernoulli_predictions = predict(train_data, test_data, 'Bernoulli')
-decision_predictions = predict(train_data, test_data, 'DecisionTree')
-logistic_predictions = predict(train_data, test_data, 'Logistic')
-majority_predictions = predict(train_data, test_data, 'Majority')
+#bernoulli_predictions = predict(train_data, test_data, 'Bernoulli')
+#decision_predictions = predict(train_data, test_data, 'DecisionTree')
+#logistic_predictions = predict(train_data, test_data, 'Logistic')
+#majority_predictions = predict(train_data, test_data, 'Majority')
 
-#with open('MultinomialResults.csv', mode = 'w') as file1:
-#    writer1 = csv.writer(file1, delimiter = ',', quotechar = '"', quoting = csv.QUOTE_MINIMAL)
-#    writer1.writerow(['id','Category'])
-#    for i in range(0, len(multinomial_predictions)):
-#        writer1.writerow([str(i), str(multinomial_predictions[i])])
+with open('MultinomialResults.csv', mode = 'w') as file1:
+    writer1 = csv.writer(file1, delimiter = ',', quotechar = '"', quoting = csv.QUOTE_MINIMAL)
+    writer1.writerow(['id','Category'])
+    for i in range(0, len(multinomial_predictions)):
+        writer1.writerow([str(i), str(multinomial_predictions[i])])
 
 #with open('BernoulliResults.csv', mode = 'w') as file2:
 #    writer2 = csv.writer(file2, delimiter = ',', quotechar = '"', quoting = csv.QUOTE_MINIMAL)
@@ -136,8 +136,8 @@ majority_predictions = predict(train_data, test_data, 'Majority')
 #    for i in range(0, len(logistic_predictions)):
 #        writer4.writerow([str(i), str(logistic_predictions[i])])
 
-with open('MaxResults.csv', mode = 'w') as file5:
-    writer5 = csv.writer(file5, delimiter = ',', quotechar = '"', quoting = csv.QUOTE_MINIMAL)
-    writer5.writerow(['id', 'Category'])
-    for i in range(0, len(majority_predictions)):
-        writer5.writerow([str(i), str(majority_predictions[i])])
+#with open('MaxResults.csv', mode = 'w') as file5:
+#    writer5 = csv.writer(file5, delimiter = ',', quotechar = '"', quoting = csv.QUOTE_MINIMAL)
+#    writer5.writerow(['id', 'Category'])
+#    for i in range(0, len(majority_predictions)):
+#        writer5.writerow([str(i), str(majority_predictions[i])])
